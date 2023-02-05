9bf63b899442138c87edcf66b61b0dc7ce90a29e
Norceis <cubix77@gmail.com>
2023-02-05 20:01:28 +0100

    base auto model working as intended

--- readme.md
+++ readme.md
@@ -1,4 +1,55 @@
-python -m spacy download pl_core_news_lg   
+Part 1 - data standardizing and preprocessing
 
-https://opus.nlpl.eu/OpenSubtitles-v2018.php opensubtitles data for bert
-https://opus.nlpl.eu/ParaCrawl.php paracrawl data for bert
+Data supplied along the task (recruitment_task.csv) had samples mostly in 2 general forms:
+1. Samples with features separated by a comma with large text features such as "Name" and "Description" having quotes inside of them 
+    as a "quotechar", which forbids data processing tools to treat commas inside those texts as separators.
+2. Samples with features separated by a comma, but also enclosed in a quote character from start to finish
+3. Outlier samples that do not belong to first 2 groups (around 200 samples, discarded)
+Having those 2 differently formatted sample groups I needed to standardize them into one format - which is the first one.
+is done in (data_standardization.ipynb), also moving the finished function to (util.py).
+
+After tackling the problem of standardizing data I have started to explore and transform the data according to the 
+problem specification (data_preprocessing.ipynb):
+1. At first I check visually if data loads properly after standardizing
+2. I study how many empty fields are in the data and also I look at different values and their
+   respective count in given column
+3. The first preprocessing step is dropping all samples where 'Price' is NaN, because those
+   are useless in this problem
+4. I filter the data to only have samples with Condition=='Używany', Type=='Sprawny', 
+   and Brand=='iPhone', according to the specification of the problem. The dataset contains approximately 2/3 of 'Używane'
+   samples, so this is the biggest sample cut off. Type and Brand filters only affect no more than few hundred samples
+5. At this point I decide to drop all the columns that do not carry important information according 
+   to the specification of the problem, e.g. 'Voivodeship' or 'URL'
+6. It is also important to cut off samples that do not offer actual product, but an accessory or a service,
+    such as phone case or a repair service - I apply this by dropping all samples below value 1000 in 'Price'
+   This is probably not the best way to do this, but I assumed that any working iPhone 11 model wouldn't be offered
+   for such a low price
+7. Converting all strings in the data to lowercase
+8. Creating new column that counts how many days passed since earliest date in the data
+9. Concatenating few columns into one, in order to pass only 1 column to the model later on (to tweak)
+
+Both notebooks resulted in 2 functions used to load data later on. Data preprocessing could be 
+more generalized and parametrized.
+
+Part 2 - NLP model
+Having processed data in form of concatenated text used to feed the model in one column and target price in other
+I proceed to build a basic model with pretrained polish language model 'dkleczek/bert-base-polish-uncased-v1' 
+from Hugging Face transformers library, which is based on BERT model from Google. 
+
+Preparation of the data for the model consists of several parts:
+1. Scaling 'Price' values to range (-1, 1)
+2. Splitting the dataset into training, validation and test groups
+3. Tokenization of each of the sets
+4. Converting tokens into torch-type dataset along with the labels
+
+Torch-type dataset is the entry to training and validation phase of the process
+
+Part 3 - Visualisation in Dash
+
+
+
+
+
+
+
+
--- src/util.py
+++ src/util.py
@@ -1,9 +1,65 @@
 import pandas as pd
-from typing import Union
+import numpy as np
+import csv
+import torch
+from typing import Union, Dict
+from transformers.trainer_utils import PredictionOutput
+from torch.utils.data import TensorDataset
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, Trainer, TrainingArguments
+def create_standardized_data(feature_number: Union[None, int] = None) -> None:
+    """
+    This function goes through each line in the dataset via "next" generator and depending
+    on the sample rewrites it, so the data is standardized (explained in detail in readme.md)
+    :param feature_number: optional, specifies number of columns, if the dataset does not contain header
+    :return: None, writes file directly to .csv
+    """
+
+    rows = []
+    with open('../data/recruitment_data.csv', 'r', encoding='utf-8') as opened_file:
+        reader = csv.reader(opened_file)
+        while True:
+            try:
+                line = next(reader)
+                if len(line) == 1:
+                    line = line[0]          # because some of the samples were enclosed in additional quote we need to
+                    rows.append(line)       # get it out of the one-element list it was put into by csv module
+                else:
+                    rows.append(line)       # rest of the samples were not modified
+            except StopIteration:
+                break
+
+    if not feature_number:
+        feature_number = len(rows[0]) # number of features present in header of csv file
+
+    with open('../data/recruitment_data_modified_python.csv', 'w', encoding='utf-8') as writing_file:
+
+        for sample in rows:
+            if len(sample) > feature_number:
+                try:
+                    writing_file.write(sample + '\n')              # outlier samples (n_features > 13) caused problems
+                except TypeError:                                  # here, this solution works correctly
+                    writing_file.write(','.join(sample) + '\n')
+            else:
+                writing_file.write(','.join(sample) + '\n')
+
+
 
 def preprocess_data(data: pd.DataFrame,
                     columns_to_drop: Union[None, list] = None,
                     price_clip: int = 1000) -> pd.DataFrame:
+    """
+    This function summarizes the exploratory work done in data_preprocessing.ipynb.
+    Due to the nature of the problem posed in this project it was suited for this particular
+    dataset, but it can be improved in more general form (TODO)
+    :param data: DataFrame with the raw data
+    :param columns_to_drop: optional, which columns to drop in order to have more compact DataFrame
+    :param price_clip: optional, used to cut off samples under this treshold in 'Price'
+    :return: processed DataFrame
+    """
+
+
     # dropping nans in price
     data_dropped_price_nans = data.dropna(subset=['Price']).reset_index(drop=True)
 
@@ -23,7 +79,7 @@ def preprocess_data(data: pd.DataFrame,
 
     data_reduced_dims = data_reduced_dims.drop(columns=columns_to_drop)
 
-    # filtering phone cases, offered services, other phones
+    # filtering phone cases, offered services, other phones based on price
     data_filtered = data_reduced_dims[data_reduced_dims.Price > price_clip]
 
     # applying lowercase to all letters in Name and Description
@@ -44,3 +100,46 @@ def preprocess_data(data: pd.DataFrame,
                                                     'First_date', 'Date', 'Days_passed']).reset_index(drop=True)
 
     return data_concatenated
+
+def compute_metrics(eval_pred: PredictionOutput) -> Dict:
+    logits, labels = eval_pred
+    labels = labels.reshape(-1, 1)
+
+    mse = mean_squared_error(labels, logits)
+    rmse = mean_squared_error(labels, logits, squared=False)
+    mae = mean_absolute_error(labels, logits)
+    r2 = r2_score(labels, logits)
+
+    return {"mse": mse, "rmse": rmse, "mae": mae, "r2": r2}
+
+class MakeTorchData(torch.utils.data.Dataset):
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {k: torch.tensor(v[idx]) for k, v in self.encodings.items()}
+        item["labels"] = torch.tensor([self.labels[idx]])
+        item["labels"] = float(item["labels"])
+        return item
+
+    def __len__(self):
+        return len(self.labels)
+def foresee(model: Trainer,
+            scaler: StandardScaler,
+            tokenizer: AutoTokenizer,
+            text: str = 'iphone 11',
+            days: tuple = (1, 90)) -> np.ndarray:
+
+    extrapolated_x = []
+    extrapolated_y = []
+    for i in range(*days):
+        extrapolated_x.append(str(i) + ' days ' + text)
+        extrapolated_y.append(0)
+
+    extrapolated_tokens = tokenizer(extrapolated_x, truncation=True, padding=True, max_length=50)
+    extrapolated_dataset = MakeTorchData(extrapolated_tokens, np.asarray(extrapolated_y).ravel())
+    extrapolated_predictions = model.predict(extrapolated_dataset)
+    inversed_extrapolated = scaler.inverse_transform(np.asarray(extrapolated_predictions[0]).reshape(-1, 1))
+
+    return inversed_extrapolated
